04a586a7bd54175b281c86438692a05ef3368355
Yann Forget <yannforget@mailbox.org>
2018-09-11 17:36:41 +0200

    Add __author__ and __version__

---
+++ pylandsat/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Yann Forget"
+__license__ = "MIT"
+__version__ = "0.1"
+
+
+from pylandsat.scene import Scene
+from pylandsat.catalog import Catalog
+from pylandsat.download import Product
